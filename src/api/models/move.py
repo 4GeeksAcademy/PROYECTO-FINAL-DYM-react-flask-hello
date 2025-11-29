851394e6448048073b3643e8c6ed26c63b8a2971
Daniela González <danimge94@gmail.com>
2025-11-29 20:36:12 +0000

    comenzando modelo move

---
+++ src/api/models/move.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+from sqlalchemy import CheckConstraint, DateTime, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from . import db
+
+
+pokemon_move = db.Table(
+    "pokemon_move",
+    db.Column("pokemon_id", ))
+)
