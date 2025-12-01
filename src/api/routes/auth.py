2cf75a72d995b26a8923642c8353b8fd660a2d89
Daniela González <danimge94@gmail.com>
2025-12-01 02:56:53 +0000

    no habia hecho la ruta de favs :D ahora sí

--- src/api/routes/auth.py
+++ src/api/routes/auth.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from flask import Blueprint, jsonify, request
 
 from api.models import Trainer, db
-from api.utils.auth import create_acces_token, login_required
+from api.utils.auth import create_access_token, login_required
 
 from api.utils.ctx import current_trainer
 
@@ -33,7 +33,7 @@ def register_trainer():
     db.session.add(trainer)
     db.session.commit()
 
-    access_token = create_acces_token(trainer)
+    access_token = create_access_token(trainer)
 
     return (
         jsonify(
@@ -64,7 +64,7 @@ def login_trainer():
     if not trainer or not trainer.check_password(password):
         return jsonify({"message": "Credenciales inválidas"}), HTTPStatus.UNAUTHORIZED
 
-    access_token = create_acces_token(Trainer)
+    access_token = create_access_token(Trainer)
     return jsonify({"trainer": trainer.serialize(), "access_token": access_token})
 
 
