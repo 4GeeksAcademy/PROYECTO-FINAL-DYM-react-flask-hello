aa3ddc25ddedcbc65c8db309a784162e073f5a4f
fer-orfao <133964332+fer-orfao@users.noreply.github.com>
2025-12-02 01:16:40 +0000

    listo

---
+++ src/front/pages/Favoritos.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+
+// Funciones LocalStorage compartidas
+const getFavorites = () => {
+    return JSON.parse(localStorage.getItem("favorites")) || [];
+};
+
+const saveFavorites = (favorites) => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+};
+
+const removeFavorite = (id) => {
+    const favs = getFavorites().filter(f => f.id !== id);
+    saveFavorites(favs);
+    return favs;
+};
+
+export const Favoritos = () => {
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        setFavorites(getFavorites());
+    }, []);
+
+    const handleRemove = (id) => {
+        const updated = removeFavorite(id);
+        setFavorites(updated);
+    };
+
+
+    
+    return (
+        <div className="container mt-4">
+            <h1 className="text-center mb-4">❤️ Tus Pokémon Favoritos</h1>
+
+            {favorites.length === 0 ? (
+                <p className="text-center fs-4">No tienes favoritos todavía.</p>
+            ) : (
+                <div className="row">
+                    {favorites.map(poke => (
+                        <div key={poke.id} className="col-6 col-md-3 col-lg-2 mb-4">
+                            <div className="card text-center p-3">
+                                <img src={poke.sprite} alt={poke.name} />
+
+                                <h5 className="mt-2 text-capitalize">{poke.name}</h5>
+                                <p>#{poke.id}</p>
+
+                                <button
+                                    className="btn btn-danger w-100 fw-bold"
+                                    onClick={() => handleRemove(poke.id)}
+                                >
+                                    💔 Quitar de favoritos
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
